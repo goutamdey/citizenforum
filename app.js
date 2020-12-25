35b8d07dde27a04bdb5d4b48aa627e6fd49ac883
Subhadip Maji <subhadipmaji93@gmail.com>
2020-12-25 09:53:11 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+/*-----------------------------------Sidebar-----------------------------------------*/
+
+
+let sidebar_btn = document.querySelector(".btn");
+let sidebar = document.querySelector(".sidebar");
+setInterval(()=>{
+    sidebar.style.height = document.body.scrollHeight + 'px';
+}, 1000)
+
+sidebar_btn.onclick = function(){
+    this.classList.toggle("click");
+    sidebar.classList.toggle("show");
+};
+
+let study_material_btn = document.querySelector(".stdy-mtrl-btn");
+study_material_btn.onclick = function(){
+    document.querySelector("nav ul .material-show").classList.toggle("show");
+    document.querySelector("nav ul .first").classList.toggle("rotate");
+};
+
+/*let activeElem = document.querySelector("nav ul li");
+ active.onclick = function(){
+    this.classList.add("active");
+    let parentElem = this.parentNode;
+    console.log(parentElem);
+};
+*/
+/*X-----------------------------------Sidebar-----------------------------------------X*/
+
+/*-------------------------------------Modal-------------------------------------------*/
+
+// Modal Element Creation
+const modalContainer = document.createElement("div");
+const modalHeader = document.createElement("div");
+const modalTitle = document.createElement("div");
+const modalCloseButton = document.createElement("button");
+const modalIframe = document.createElement("iframe");
+const overlay = document.createElement("div");
+const downloadButton = document.createElement("button");
+
+modalContainer.classList.add("modal");
+modalContainer.id = "modal";
+
+modalHeader.classList.add("modal-header");
+modalTitle.classList.add("title");
+
+modalCloseButton.classList.add("close-button");
+modalCloseButton.innerHTML = "&times;"
+
+modalIframe.classList.add("modal-body");
+overlay.id = "overlay";
+
+downloadButton.innerText = "Download";
+downloadButton.id = "dwnld-btn";
+
+// Attach Element
+modalHeader.appendChild(modalTitle);
+modalHeader.appendChild(downloadButton);
+modalHeader.appendChild(modalCloseButton);
+
+modalContainer.appendChild(modalHeader);
+modalContainer.appendChild(modalIframe);
+
+document.body.appendChild(overlay);
+
+
+document.body.addEventListener("click", function(e){
+    if(e.target.hasAttribute("data-doc-id")){
+        modalTitle.textContent = e.target.innerText;
+        modalIframe.src = `https://drive.google.com/file/d/${e.target.getAttribute("data-doc-id")}/preview`;
+    
+        downloadButton.onclick = function(){
+            window.location = `https://drive.google.com/uc?export=download&id=${e.target.getAttribute("data-doc-id")}`;
+        };
+        overlay.classList.add("active");
+        document.body.appendChild(modalContainer);
+    } else {
+        return;
+    }
+});
+
+modalCloseButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+function closeModal(e){
+    document.body.removeChild(modalContainer);
+    overlay.classList.remove('active');
+    if(e.target.classList.contains("dropbox-embed")) e.target.classList.remove("dropbox-embed");	  
+}
+
+/*X-------------------------------------Modal-------------------------------------------X*/
